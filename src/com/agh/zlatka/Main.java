22897d1f0269f7bb8f92ffd83b46cfe9c07356f3
zbiglatka <zbiglatka@wp.pl>
2015-11-19 14:35:03 +0100

    Initial commit

---
+++ src/com/agh/zlatka/Main.java
@@ -0,0 +1,9 @@
+package com.agh.zlatka;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("analizator wersja 1");
+    }
+}
