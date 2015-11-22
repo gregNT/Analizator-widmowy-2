b34457dd54ceb285ad7f29ee0c118a6aad5eabfe
gregNT <gregxxxnt@o2.pl>
2015-11-22 18:52:30 +0100

    STFT: - Znalazłem gotową bibliotekę do obliczania STFT (link w komentarzu w STFT.java)

--- src/com/agh/zlatka/SFFT.java
+++ src/com/agh/zlatka/SFFT.java
@@ -1,6 +1,10 @@
 package com.agh.zlatka;
 
 
+// Pomysł - nie trzeba się bawić z liczbami urojonymi, bo wystarczy
+// przechowywać tablicę double [] [] z KWADRATAMI amplitud.
+// Najprościej będzie wykorzystać klasę dr Flanagana - są tam gotowe funkcje do obliczenia
+// macierzy STFT (http://www.ee.ucl.ac.uk/~mflanaga/java/FourierTransform.html#class)
 public class SFFT {
 
     private
@@ -19,11 +23,14 @@ public class SFFT {
 
         K=Math.floor((y.length-N)/(N*(1-overlap))+1);
 
+        /*
+
         for (int i=1; i<K+1; i++){
 
             // FT = fft(y[N(i-1) : N*i-1]*okno);
             // SFFT = [SFFT ; FT]
         }
+        */
 
     }
 }
